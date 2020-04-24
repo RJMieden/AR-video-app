30773deee7afc664bde76ea6c348eb8efddfa599
Yuri van Geffen <yuri@yurivangeffen.com>
2020-04-24 14:33:57 +0200

    Added multi-image support.

--- Livecrowd AR/ViewController.swift
+++ Livecrowd AR/ViewController.swift
@@ -35,7 +35,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
             // if there is, set the images to track
             configuration.trackingImages = trackedImages
             // at any point in time, only 1 image will be tracked
-            configuration.maximumNumberOfTrackedImages = 1
+            configuration.maximumNumberOfTrackedImages = trackedImages.count
         }
         
         // Run the view's session
@@ -54,40 +54,44 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
         
         // if the anchor is not of type ARImageAnchor (which means image is not detected), just return
-        guard let imageAnchor = anchor as? ARImageAnchor, let fileUrlString = Bundle.main.path(forResource: "black", ofType: "mp4") else {return}
-        //find our video file
-        
-        let videoItem = AVPlayerItem(url: URL(fileURLWithPath: fileUrlString))
-        
-        let player = AVPlayer(playerItem: videoItem)
-        //initialize video node with avplayer
-        let videoNode = SKVideoNode(avPlayer: player)
-        
-        player.play()
-        // add observer when our player.currentItem finishes player, then start playing from the beginning
-        NotificationCenter.default.addObserver(forName: .AVPlayerItemDidPlayToEndTime, object: player.currentItem, queue: nil) { (notification) in
-            player.seek(to: CMTime.zero)
+        if let imageAnchor = anchor as? ARImageAnchor {
+            let imageName = imageAnchor.referenceImage.name!;
+            print("IMAGENAME:", imageName);
+            guard let fileUrlString = Bundle.main.path(forResource: imageName, ofType: "mp4") else {return}
+            
+            //find our video file
+            let videoItem = AVPlayerItem(url: URL(fileURLWithPath: fileUrlString))
+            
+            let player = AVPlayer(playerItem: videoItem)
+            //initialize video node with avplayer
+            let videoNode = SKVideoNode(avPlayer: player)
+            
             player.play()
-            print("Looping Video")
+            // add observer when our player.currentItem finishes player, then start playing from the beginning
+            NotificationCenter.default.addObserver(forName: .AVPlayerItemDidPlayToEndTime, object: player.currentItem, queue: nil) { (notification) in
+                player.seek(to: CMTime.zero)
+                player.play()
+                print("Looping Video")
+            }
+            
+            // set the size (just a rough one will do)
+            let videoScene = SKScene(size: CGSize(width: 480, height: 360))
+            // center our video to the size of our video scene
+            videoNode.position = CGPoint(x: videoScene.size.width / 2, y: videoScene.size.height / 2)
+            // invert our video so it does not look upside down
+            videoNode.yScale = -1.0
+            // add the video to our scene
+            videoScene.addChild(videoNode)
+            // create a plan that has the same real world height and width as our detected image
+            let plane = SCNPlane(width: imageAnchor.referenceImage.physicalSize.width, height: imageAnchor.referenceImage.physicalSize.height)
+            // set the first materials content to be our video scene
+            plane.firstMaterial?.diffuse.contents = videoScene
+            // create a node out of the plane
+            let planeNode = SCNNode(geometry: plane)
+            // since the created node will be vertical, rotate it along the x axis to have it be horizontal or parallel to our detected image
+            planeNode.eulerAngles.x = -Float.pi / 2
+            // finally add the plane node (which contains the video node) to the added node
+            node.addChildNode(planeNode)
         }
-        
-        // set the size (just a rough one will do)
-        let videoScene = SKScene(size: CGSize(width: 480, height: 360))
-        // center our video to the size of our video scene
-        videoNode.position = CGPoint(x: videoScene.size.width / 2, y: videoScene.size.height / 2)
-        // invert our video so it does not look upside down
-        videoNode.yScale = -1.0
-        // add the video to our scene
-        videoScene.addChild(videoNode)
-        // create a plan that has the same real world height and width as our detected image
-        let plane = SCNPlane(width: imageAnchor.referenceImage.physicalSize.width, height: imageAnchor.referenceImage.physicalSize.height)
-        // set the first materials content to be our video scene
-        plane.firstMaterial?.diffuse.contents = videoScene
-        // create a node out of the plane
-        let planeNode = SCNNode(geometry: plane)
-        // since the created node will be vertical, rotate it along the x axis to have it be horizontal or parallel to our detected image
-        planeNode.eulerAngles.x = -Float.pi / 2
-        // finally add the plane node (which contains the video node) to the added node
-        node.addChildNode(planeNode)
     }
 }
